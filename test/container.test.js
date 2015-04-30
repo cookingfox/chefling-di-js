edc1536083b7c6a60a623d9411d8b0d93c7598d6
Abel de Beer <abel@cookingfox.nl>
2015-04-30 21:16:33 +0200

    WIP: create, get, has, remove, reset

--- test/container.test.js
+++ test/container.test.js
@@ -3,7 +3,7 @@
 describe('container', function () {
 
     //--------------------------------------------------------------------------
-    // TEST SUITE
+    // SUITE
     //--------------------------------------------------------------------------
 
     var container = null;
@@ -16,8 +16,84 @@ describe('container', function () {
         container = null;
     });
 
-    it('should create a Container instance', function () {
+    it('suite - should initialize the container', function () {
         assert.instanceOf(container, Container);
     });
 
+    //--------------------------------------------------------------------------
+    // CREATE
+    //--------------------------------------------------------------------------
+
+    it('create - should create an instance of type', function () {
+        var result = container.create(NoParameterConstructor);
+
+        assert.instanceOf(result, NoParameterConstructor);
+    });
+
+    //--------------------------------------------------------------------------
+    // GET
+    //--------------------------------------------------------------------------
+
+    it('get - should create an instance of type', function () {
+        var result = container.get(NoParameterConstructor);
+
+        assert.instanceOf(result, NoParameterConstructor);
+    });
+
+    it('get - should return same instance', function () {
+        var result1 = container.get(NoParameterConstructor);
+        var result2 = container.get(NoParameterConstructor);
+
+        assert.strictEqual(result2, result1);
+    });
+
+    //--------------------------------------------------------------------------
+    // HAS
+    //--------------------------------------------------------------------------
+
+    it('has - should return false if no value', function () {
+        var result = container.has(NoParameterConstructor);
+
+        assert.isFalse(result);
+    });
+
+    it('has - should return false if no value', function () {
+        container.get(NoParameterConstructor);
+        var result = container.has(NoParameterConstructor);
+
+        assert.isTrue(result);
+    });
+
+    //--------------------------------------------------------------------------
+    // REMOVE
+    //--------------------------------------------------------------------------
+
+    it('remove - should remove the stored instance', function () {
+        container.get(NoParameterConstructor);
+        container.remove(NoParameterConstructor);
+        var result = container.has(NoParameterConstructor);
+
+        assert.isFalse(result);
+    });
+
+    //--------------------------------------------------------------------------
+    // RESET
+    //--------------------------------------------------------------------------
+
+    it('reset - should remove all stored values', function () {
+        container.get(NoParameterConstructor);
+        container.reset();
+
+        var result = container.has(NoParameterConstructor);
+
+        assert.isFalse(result);
+    });
+
 });
+
+//------------------------------------------------------------------------------
+// FIXTURES
+//------------------------------------------------------------------------------
+
+function NoParameterConstructor() {
+}
